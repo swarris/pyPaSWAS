2a0725e5c416738d8fc34f5277bf58a7926a6e60
Sven Warris <sven@warris.nl>
2015-01-03 00:13:31 +0100

    Added a basic wrapper, making the -m option unnecessary

---
+++ pypaswas.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from pyPaSWAS.pypaswasall import Pypaswas
+
+if __name__ == '__main__':
+    try:
+        ppw = Pypaswas()
+        ppw.run()
+    except Exception as exception:
+        # Show complete exception when running in DEBUG
+        if (hasattr(ppw.settings, 'loglevel') and
+            getattr(logging, 'DEBUG') == ppw.logger.getEffectiveLevel()):
+            ppw.logger.exception(str(exception))
+        else:
+            print 'Something unforeseen caused the program to terminate.\n' \
+                  'The internal error message was: ', ','.join(exception.args)
