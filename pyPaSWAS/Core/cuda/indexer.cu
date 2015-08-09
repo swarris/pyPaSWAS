5677dead5b75722594540ea1bf8e8cb34d0d47df
Sven Warris <sven@warris.nl>
2015-08-09 23:48:36 +0200

    CUDA/OpenCL indexer source code

---
+++ pyPaSWAS/Core/cuda/indexer.cu
@@ -0,0 +1,36 @@
+#include <math.h>
+#include <builtin_types.h>
+
+#define INDEX_SIZE ${size}
+#define BLOCK_SIZE ${block}
+
+
+extern "C"
+__global__ void calculateDistance(int *index, int *query, float *distances, float scale);
+
+__global__ void calculateDistance(int *index, int *query, float *distances, float scale){
+
+	__shared__ float s_distances[INDEX_SIZE];
+
+	unsigned int block = blockIdx.x * BLOCK_SIZE + blockIdx.y;
+	unsigned int threadPlus1 = threadIdx.x+1;
+	unsigned int thread = threadIdx.x;
+	s_distances[thread] = (float) index[block+threadPlus1] - (float)query[threadPlus1];
+	s_distances[thread] *= s_distances[thread];
+
+
+	unsigned int offset = 1;
+
+	for (int i=INDEX_SIZE>>1; i > 0 ; i >>=1 ) {
+		__syncthreads();
+		if (thread < i) {
+		    int ai = offset*(2*thread+1)-1;
+		    int bi = offset*(2*thread+2)-1;
+		    s_distances[bi] += s_distances[ai];
+		}
+		offset *= 2;
+	}
+	if (thread == 0){
+		distances[block] =  sqrt(s_distances[INDEX_SIZE-1])/scale;
+	}
+}
---
+++ pyPaSWAS/Core/ocl/indexer.cl
@@ -0,0 +1,33 @@
+#include <math.h>
+#include <builtin_types.h>
+
+#define INDEX_SIZE ${size}
+#define BLOCK_SIZE ${block}
+
+
+__kernel void calculateDistance(__global int *index, __global int *query, __global float *distances, float scale){
+
+	__local float s_distances[INDEX_SIZE];
+
+	unsigned int block = get_group_id(0) * BLOCK_SIZE + get_group_id(1);
+	unsigned int threadPlus1 = get_local_id(0)+1;
+	unsigned int thread = get_local_id(0);
+	s_distances[thread] = (float) index[block+threadPlus1] - (float)query[threadPlus1];
+	s_distances[thread] *= s_distances[thread];
+
+
+	unsigned int offset = 1;
+
+	for (int i=INDEX_SIZE>>1; i > 0 ; i >>=1 ) {
+		barrier(CLK_LOCAL_MEM_FENCE);
+		if (thread < i) {
+		    int ai = offset*(2*thread+1)-1;
+		    int bi = offset*(2*thread+2)-1;
+		    s_distances[bi] += s_distances[ai];
+		}
+		offset *= 2;
+	}
+	if (thread == 0){
+		distances[block] =  sqrt(s_distances[INDEX_SIZE-1])/scale;
+	}
+}
