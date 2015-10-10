d5f27aabbfbb70bae59265ecbf83ee278181522e
Sven Warris <sven@warris.nl>
2015-10-11 00:41:55 +0200

    Fixed stepping to far in genome with windows

--- pyPaSWAS/Core/QIndexerCUDA.py
+++ pyPaSWAS/Core/QIndexerCUDA.py
@@ -125,47 +125,50 @@ class QIndexerCUDA(QIndexer):
                         startWindow = self.indicesStep - self.indexCount
                     if startWindow < 0 :
                         numberOfWindowsToCalculate += startWindow
-                        startWindow = 0        
-                    self.indexCount += startWindow + numberOfWindowsToCalculate
-                    numberOfWindowsInPrevSeqs += numberOfWindowsToCalculate 
-                
-                    self.logger.debug("Creating index of {} with window {}".format(sequence[seqId].id, window))
-                    self.logger.debug("Will process #windows: {}".format(numberOfWindowsToCalculate))
-                    startIndex = startWindow * revWindowSize
-                    endIndex = numberOfWindowsToCalculate * revWindowSize + startIndex + window
-                    seqToIndex = str(sequence[seqId].seq[startIndex:endIndex]).upper() 
-                    self.logger.debug("Indices: {}, {}".format(startIndex, endIndex)) 
-                    # memory on gpu for count should already be enough, so copy sequence to gpu
-                    seqHost = numpy.array(seqToIndex, dtype=numpy.character)
-                    seqMem = driver.pagelocked_empty((len(seqToIndex), 1), numpy.byte, mem_flags=driver.host_alloc_flags.DEVICEMAP) 
-                    seqGPU = numpy.intp(seqMem.base.get_device_pointer()) #driver.mem_alloc(len(seqToIndex)) #
-                    driver.memcpy_htod(seqGPU, seqHost)
+                        startWindow = 0
+                    if numberOfWindowsToCalculate > 0:        
+                        self.indexCount += startWindow + numberOfWindowsToCalculate
+                        numberOfWindowsInPrevSeqs += numberOfWindowsToCalculate 
                     
-                    # set comps to zero
-                    dim_grid = (self.indicesStepSize/self.block, self.block,1)
-                    dim_block = (len(self.character_list), 1,1)
-                    self.setToZero_function(self.d_compAll_index, block=dim_block, grid=dim_grid)
-                    driver.Context.synchronize() 
-                    # perform count on gpu 
-                    dim_grid = (int(math.ceil(len(seqToIndex)/float(len(self.character_list)))), 1,1)
-                    dim_block = (len(self.character_list), 1,1)
-                    self.logger.debug("Calculating qgram per location in sequence. q={}, length={}, block={}, grid={}".format(
-                                self.qgram, len(seqToIndex), dim_block, dim_grid))
-                    self.calculate_qgrams_function(seqGPU, numpy.int32(self.qgram), numpy.int32(len(seqToIndex)), self.d_compAll_index,
-                                    numpy.float32(window), numpy.float32(revWindowSize), numpy.float32(self.compositionScale / float(window-self.qgram+1)), 
-                                     block=dim_block, 
-                                     grid=dim_grid)
-                    driver.Context.synchronize() 
-                    comps = numpy.ndarray(buffer=self.h_compAll_index, dtype=numpy.float32, shape=(1,len(self.h_compAll_index)))[0].tolist()
-                    # add comps to tuple set
-                    for w in xrange(numberOfWindowsToCalculate):
-                        count = tuple(comps[w*(len(self.character_list)+1):(w+1)*(len(self.character_list)+1)])
-                        if count not in self.tupleSet:
-                            self.tupleSet[count] = [] 
-                        self.tupleSet[count].append((w*revWindowSize, seqId))
+                        self.logger.debug("Creating index of {} with window {}".format(sequence[seqId].id, window))
+                        self.logger.debug("Will process #windows: {}".format(numberOfWindowsToCalculate))
+                        startIndex = startWindow * revWindowSize
+                        endIndex = numberOfWindowsToCalculate * revWindowSize + startIndex + window
+                        seqToIndex = str(sequence[seqId].seq[startIndex:endIndex]).upper() 
+                        self.logger.debug("Indices: {}, {}".format(startIndex, endIndex)) 
+                        # memory on gpu for count should already be enough, so copy sequence to gpu
+                        seqHost = numpy.array(seqToIndex, dtype=numpy.character)
+                        seqMem = driver.pagelocked_empty((len(seqToIndex), 1), numpy.byte, mem_flags=driver.host_alloc_flags.DEVICEMAP) 
+                        seqGPU = numpy.intp(seqMem.base.get_device_pointer()) #driver.mem_alloc(len(seqToIndex)) #
+                        driver.memcpy_htod(seqGPU, seqHost)
+                        
+                        # set comps to zero
+                        dim_grid = (self.indicesStepSize/self.block, self.block,1)
+                        dim_block = (len(self.character_list), 1,1)
+                        self.setToZero_function(self.d_compAll_index, block=dim_block, grid=dim_grid)
+                        driver.Context.synchronize() 
+                        # perform count on gpu 
+                        dim_grid = (int(math.ceil(len(seqToIndex)/float(len(self.character_list)))), 1,1)
+                        dim_block = (len(self.character_list), 1,1)
+                        self.logger.debug("Calculating qgram per location in sequence. q={}, length={}, block={}, grid={}".format(
+                                    self.qgram, len(seqToIndex), dim_block, dim_grid))
+                        self.calculate_qgrams_function(seqGPU, numpy.int32(self.qgram), numpy.int32(len(seqToIndex)), self.d_compAll_index,
+                                        numpy.float32(window), numpy.float32(revWindowSize), numpy.float32(self.compositionScale / float(window-self.qgram+1)), 
+                                         block=dim_block, 
+                                         grid=dim_grid)
+                        driver.Context.synchronize() 
+                        comps = numpy.ndarray(buffer=self.h_compAll_index, dtype=numpy.float32, shape=(1,len(self.h_compAll_index)))[0].tolist()
+                        # add comps to tuple set
+                        for w in xrange(numberOfWindowsToCalculate):
+                            count = tuple(comps[w*(len(self.character_list)+1):(w+1)*(len(self.character_list)+1)])
+                            if count not in self.tupleSet:
+                                self.tupleSet[count] = [] 
+                            self.tupleSet[count].append((w*revWindowSize, seqId))
+                        
                     
-                
-                    currentTupleSet.update(self.tupleSet)
+                        currentTupleSet.update(self.tupleSet)
+                    else:
+                        self.indexCount += self.indicesStepSize
                 seqId += 1
                 
         self.indicesStep += self.indicesStepSize
